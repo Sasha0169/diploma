196cce50f76ac5e2ec32ed4ecdac8b5e3a2efbd5
Alexandr <sasha0169s@mail.ru>
2025-03-28 01:17:11 +0300

    Добавлены все файлы

--- app.js
+++ app.js
@@ -23,6 +23,14 @@ app.listen(3000, () => {
       res.render("index", {});
   });
 
+  app.get("/cruise", (req, res) => {
+    res.render("cruise page", {});
+});
+
+  app.get("/booking-form", (req, res)=>{
+    res.render("booking form", {})
+  })
+
   const { Pool } = require('pg');
 
 const pool = new Pool({
@@ -35,7 +43,7 @@ const pool = new Pool({
 
 pool.connect()
   .then(() =>{ console.log('✅ Успешное подключение к PostgreSQL');
-    a();
+    getCruises("Красноярск", ["По Енисею", "По Волге"], {startDate:"21.03.2025", endDate:"30.03.2025"}, ["1 - 4", "8 - 10"], ["Максим Горький", "Бирюса (СВП)"], "Речной").then(result => console.log(result));
   })
   .catch(err => console.error('❌ Ошибка подключения к PostgreSQL:', err));
 
@@ -58,11 +66,101 @@ app.get('/users', async (req, res) => {
     }
 });
 
-function a(){
+// async function getInfoAboutCruises(){
+//     try {
+//         const result = await pool.query('SELECT * FROM public."cabins"'); // SQL-запрос
+//         console.log(result.rows);
+//         // console.log(result.rows[0].arrival_location.port)
+//     } catch (error) {
+//         console.error(error);
+//     }
+// }
+
+async function getCruise(){
+
+}
+
+
+async function getCruises(placeOfDeparture, routes, dates, numberOfDay, liners, type){
     try {
-        const result = pool.query('SELECT * FROM public."Users"'); // SQL-запрос
-        console.log(result.rows);
+      let query = `
+        SELECT cruise_id, cruise_name, route_points, start_date, end_date, (end_date - start_date) AS duration_days, ship_id 
+        FROM public."cruises"
+       `;
+      let additionalQuery;
+      let routesId=[];
+      let endOfQuery='';
+
+
+      if(placeOfDeparture != undefined)
+        endOfQuery += ` WHERE departure_location::jsonb @> \'{"port": {"city": "${placeOfDeparture}"}}\'`;
+
+
+      if(routes != undefined){
+        routes = routes.map(route=> `\'${route}\'`);
+        let stringOfRoutes = routes.join(", ");
+        additionalQuery =   ` 
+          SELECT id 
+          FROM public.routes  
+          WHERE name IN (${stringOfRoutes});`;
+        routesId = await pool.query(additionalQuery); 
+        routesId = routesId.rows.map(routeId=> `${routeId.id}`)
+        let sringOfRoutesId = routesId.join(", ");
+        endOfQuery += endOfQuery!=""? " AND ": " WHERE ";
+        endOfQuery += `route_id IN (${sringOfRoutesId})`;
+      }
+
+
+      if(dates != undefined){
+        let {startDate, endDate} = dates;
+        startDate = convertDateFormat(startDate);
+        endDate = convertDateFormat(endDate);
+        endOfQuery += endOfQuery!=""? " AND ": " WHERE ";
+        endOfQuery += `start_date BETWEEN '${startDate}' AND '${endDate}'`;
+      }
+
+
+      if(numberOfDay != undefined){
+        endOfQuery += endOfQuery!=""? " AND ": " WHERE ";
+        for(let i = 0; i < numberOfDay.length; i++){
+          numberOfDay[i] = numberOfDay[i].split(" - ");
+          if(i>=1)
+            endOfQuery += " OR "
+          endOfQuery += `((end_date - start_date) BETWEEN INTERVAL '${numberOfDay[i][0]} days' AND INTERVAL '${numberOfDay[i][1]} days')`;
+        }
+      }
+
+
+      if(liners != undefined){
+        liners = liners.map(liner=> `\'${liner}\'`);
+        let stringOfLiners = liners.join(", ");
+        additionalQuery =   ` 
+          SELECT ship_id 
+          FROM public.ships  
+          WHERE ship_name IN (${stringOfLiners});`;
+        linersId = await pool.query(additionalQuery); 
+        linersId = linersId.rows.map(routeId=> `${routeId.ship_id}`)
+        let stringOfLinersID = linersId.join(", ");
+        endOfQuery += endOfQuery!=""? " AND ": " WHERE ";
+        endOfQuery += `ship_id IN (${stringOfLinersID})`;
+      }
+      
+
+      if(type != undefined){
+        endOfQuery += endOfQuery!=""? " AND ": " WHERE ";
+        endOfQuery += `cruise_type = '${type}'`;
+      }
+
+
+      query += endOfQuery;
+      const result = await pool.query(query); 
+      return result.rows;
     } catch (error) {
         console.error(error);
     }
+}
+
+function convertDateFormat(dateStr) {
+  const [day, month, year] = dateStr.split('.');
+  return `${year}-${month}-${day}`;
 }
--- scripts/script.js
+++ scripts/script.js
@@ -38,3 +38,70 @@ checkboxes.forEach((a)=>a.addEventListener("change",(e)=>{
     else    
         label.textContent = button.values[0];
 }))
+
+let open_panel;
+let interaction_container;
+let descendants_of_objects = ["navigation-panel__column", "navigation-panel__title", "navigation-panel__option", "navigation-panel__wrap-for-content", "categories-navigation__link"];
+let elements = document.getElementsByClassName('categories-navigation__option');
+let panels = document.getElementsByClassName('navigation-panel');
+for(let i = 0, a = 0; i<elements.length; i++)
+{
+    if(i==2)
+    continue;
+    elements[i].onmouseover= open_navigation_panel;
+    elements[i].panel_for_open = panels[a];
+    elements[i].onmouseout = close_navigation_panel;
+    elements[i].panel_for_open.onmouseout = close_navigation_panel;
+    a++;
+}
+
+function open_navigation_panel(event)
+{
+    if(open_panel!=undefined)
+        if(open_panel != event.currentTarget.panel_for_open)
+        {
+            for(let i = 0; i<panels.length; i++)
+            {
+                panels[i].style.display = "none";
+            }
+        }
+    event.currentTarget.panel_for_open.style.display = "flex";
+    open_panel = event.currentTarget.panel_for_open;
+    interaction_container = event.currentTarget;
+}
+
+function close_navigation_panel(event)
+{
+    let isAnotherObject=true;
+    let relatedTarget = event.relatedTarget;
+    if(relatedTarget==null||open_panel==undefined)
+    return;
+    for(let a of descendants_of_objects)
+    {
+        if (relatedTarget.className == a)
+        {
+            isAnotherObject = false;
+            return;
+        }
+    }
+
+    if(relatedTarget==open_panel||relatedTarget==interaction_container)
+    {
+        isAnotherObject = false;
+    }
+
+    if(isAnotherObject)
+        open_panel.style.display = "none";
+}
+
+function viewMenuNavigation()
+{
+    document.getElementsByClassName('transparent-background-with-navigation-menu')[0].style.display='flex';
+    document.body.style.overflow = 'hidden';
+};
+
+function closeMenuNavigation()
+{
+    document.getElementsByClassName('transparent-background-with-navigation-menu')[0].style.display ='none';
+    document.body.style.overflow = 'scroll';
+}
